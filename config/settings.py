52e2c01e48eb0f7d74a364bf351dc4b542e8bac2
koskin <kostia.zeykin@gmail>
2025-07-30 13:06:26 +0300

    add .pyrhon-vesion + Dockerfile + PostgreSQL adapter and version in requirements + DB in settings

--- config/settings.py
+++ config/settings.py
@@ -72,10 +72,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {   # TODO: Update this section with our database configuration
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
