a83f2be0045ee5f0b28e59f0d67cd74e9280ceea
DemaPy <benderdema@gmail.com>
2025-07-31 12:25:50 +0200

    feat: add new pages to router

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import { Header } from "./components/composed/Header";
 
 export function Root() {
   return (
     <main>
       <Header />
+      <Outlet />
     </main>
   );
 }
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Root } from "../Root";
+import { About } from "@/pages/About";
+import { EnterprisesAndIndustries } from "@/pages/EnterprisesAndIndustries";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [],
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/enterprises-and-industries",
+        element: <EnterprisesAndIndustries />,
+      },
+    ],
   },
 ]);
 
