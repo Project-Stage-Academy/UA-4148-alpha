9921d5ac68ff9ffa7b8e1eb9122cd15039e3b25d
Qellexi <qellexi@gmail.com>
2025-08-01 10:24:08 +0200

    Add initial migrations for profiles and projects apps

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Startup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='startups')
+    company_name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    views_count = models.IntegerField(default=0)
+    field_of_activity = models.IntegerField(default=0)
+    location = models.IntegerField(default=0)
+    startup_logo = models.ImageField(upload_to='logos/startup_logos/', blank=True)
+
+    def __str__(self):
+        return self.company_name
+
+class Investor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='investors')
+    company_name = models.CharField(max_length=150)
+    website = models.URLField(blank=True)
+    field_of_activity = models.IntegerField(default=0)
+    location_id = models.IntegerField(default=0)
+    investor_logo = models.ImageField(upload_to='logos/investor_logos/', blank=True)
+
+    def __str__(self):
+        return self.company_name
--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from profiles.models import Startup, Investor
 
 # Create your models here.
+class ProjectStatus(models.Model):
+    status = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.status
+
+class StartupProject(models.Model):
+    startup = models.ForeignKey(Startup, on_delete=models.CASCADE, related_name='projects')
+    subject = models.CharField(max_length=150)
+    idea = models.TextField()
+    description = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+
+    investment_needed = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    status = models.ForeignKey(ProjectStatus, on_delete=models.SET_NULL, related_name='status')
+
+    def __str__(self):
+        return self.subject
+
+class ProjectFile(models.Model):
+    project = models.ForeignKey(StartupProject, on_delete=models.CASCADE, related_name='files')
+    file_url = models.FileField(upload_to='project_files/')
+    file_type = models.CharField(max_length=50)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.file_type} for {self.project.subject}"
