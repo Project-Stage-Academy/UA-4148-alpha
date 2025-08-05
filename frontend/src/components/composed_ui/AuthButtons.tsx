f2803bb400f4bdbeb61988e99348e8396131fc3b
DemaPy <benderdema@gmail.com>
2025-08-05 19:24:16 +0200

    feat: add auth routes

--- frontend/src/components/composed_ui/AuthButtons.tsx
+++ frontend/src/components/composed_ui/AuthButtons.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 
 export function AuthButtons() {
   return (
     <div className="flex items-center gap-6">
-      <Button variant={"tertiary"}>Увiйти</Button>
-      <Button>Зареєструватися</Button>
+      <Button variant={"tertiary"} asChild>
+        <Link to="/signin">Увiйти</Link>
+      </Button>
+      <Button asChild>
+        <Link to="/signup">Зареєструватися</Link>
+      </Button>
     </div>
   );
 }
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Root } from "../Root";
 import { About } from "@/pages/About";
 import { EnterprisesAndIndustries } from "@/pages/EnterprisesAndIndustries";
+import { SignIn } from "@/pages/auth/SignIn";
+import { SignUp } from "@/pages/auth/SignUp";
 
 const routes = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ const routes = createBrowserRouter([
       {
         path: "/enterprises-and-industries",
         element: <EnterprisesAndIndustries />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
       },
     ],
   },
