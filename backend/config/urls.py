f10d6bede97510e048a348f8ecb17b29039e9643
Vlad <daniil.medencev01@gmail.com>
2025-08-05 20:07:59 +0300

    Move api-token-auth route to users.urls to follow project structure

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,10 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from users.views import UserViewSet
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.authtoken import views as authtoken_views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/v1/projects/', include('projects.urls')),
-    path('api/v1/api-token-auth/', authtoken_views.obtain_auth_token, name='api-token-auth'), 
+    path('api/v1/users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
