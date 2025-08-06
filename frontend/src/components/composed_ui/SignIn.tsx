b614f51a6bd664b5f23edea0d5e0067b6e51dbad
DemaPy <benderdema@gmail.com>
2025-08-06 09:47:04 +0200

    feat: implement signin form

---
+++ frontend/src/components/composed_ui/SignIn.tsx
@@ -0,0 +1,63 @@
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "../ui/input";
+import { Link } from "react-router-dom";
+import type { UseFormReturn } from "react-hook-form";
+import type { SignInFormValues } from "@/pages/auth/SignIn";
+
+interface SignInFormProps {
+  form: UseFormReturn<SignInFormValues>;
+}
+
+export function SignInForm({ form }: SignInFormProps) {
+  return (
+    <div className="flex flex-col gap-6">
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem className="gap-1">
+            <FormLabel>Електронна пошта</FormLabel>
+            <FormControl>
+              <Input
+                className="shadow-none border border-inactive-100 rounded-sm flex focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-[3px]"
+                placeholder="Введіть свою електронну пошту"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <div className="flex flex-col gap-2">
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="gap-1">
+              <FormLabel>Пароль</FormLabel>
+              <FormControl>
+                <Input
+                  className="shadow-none border border-inactive-100 rounded-sm flex focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-[3px]"
+                  placeholder="Введіть пароль"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button variant={"tertiary"} asChild className="self-end">
+          <Link to="/forgot-password">Забули пароль?</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/auth/SignIn.tsx
+++ frontend/src/pages/auth/SignIn.tsx
@@ -1,6 +1,66 @@
+import { Form } from "@/components/composed_ui/Form";
+import { SignInForm } from "@/components/composed_ui/SignIn";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSignInMutate } from "@/hooks/useSignInMutate";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "@/hooks/useAuthContext";
+import { useEffect } from "react";
+
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email("Введіть адресу електронної пошти у форматі name@example.com"),
+  password: z.string().min(6, "Електронна пошта чи пароль вказані некоректно"),
+});
+
+export type SignInFormValues = z.infer<typeof signInSchema>;
 
 export function SignIn() {
-    return (
-        <div>SignIn</div>
-    )
+  const navigate = useNavigate();
+  const auth = useAuthContext();
+  const signIn = useSignInMutate();
+  const form = useForm<SignInFormValues>({
+    resolver: zodResolver(signInSchema),
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    if (auth?.user) {
+      navigate("/profile");
+    }
+  }, [auth?.user, navigate]);
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const handleSignIn = async (data: SignInFormValues) => {
+    try {
+      await signIn.mutateAsync(data);
+    } catch (error: unknown) {
+      form.setError("password", {
+        type: "manual",
+        message: error instanceof Error ? error.message : "Невідома помилка",
+      });
+    }
+  };
+
+  return (
+    <div className="px-2 mx-auto flex items-center justify-center h-screen">
+      <Form form={form} onSubmit={handleSignIn}>
+        <Form.Header title="Вхід на платформу" />
+        <Form.Body>
+          <SignInForm form={form} />
+        </Form.Body>
+        <Form.Footer>
+          <Button disabled={!isValid || isSubmitting}>Увійти</Button>
+        </Form.Footer>
+      </Form>
+    </div>
+  );
 }
