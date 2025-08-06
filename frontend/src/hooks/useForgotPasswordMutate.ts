5f3efa7cae835723e50229a889113e0dffadc22b
DemaPy <benderdema@gmail.com>
2025-08-06 12:11:53 +0200

    feat: add errors handling for hooks

--- frontend/src/hooks/useForgotPasswordMutate.ts
+++ frontend/src/hooks/useForgotPasswordMutate.ts
@@ -7,14 +7,21 @@ export function useForgotPasswordMutate() {
   const navigate = useNavigate();
   return useMutation<
     ResetPasswordResponse,
-    AxiosError<{ detail: string }>,
+    AxiosError<{ email: string[] }>,
     ResetPasswordData
   >({
     mutationFn: forgotPassword,
     onSuccess: () => {
       navigate("/password-recovery-email-sent");
     },
-    onError: () => {
+    onError: (error) => {
+      console.error(error);
+      if (error.response?.data?.email) {
+        throw new Error("Введіть адресу електронної пошти у форматі name@example.com");
+      }
+      if (error.response?.status === 500) {
+        throw new Error("Серверна помилка, спробуйте пізніше");
+      }
       throw new Error("Зазначена електронна адреса не зареєстрована");
     },
   });
