bbe52dcff920b42459039d2f67a5d82103fdfbe5
DemaPy <benderdema@gmail.com>
2025-08-06 20:38:33 +0200

    feat: add RestorePassword components

---
+++ frontend/src/pages/auth/RestorePassword.tsx
@@ -0,0 +1,81 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/composed_ui/Form";
+import { RestorePasswordForm } from "@/components/composed_ui/RestorePassword";
+import { Button } from "@/components/ui/button";
+import { useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useValidToken } from "@/hooks/useValidToken";
+
+const passwordRequirements =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
+
+const restorePasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, "Пароль повинен містити щонайменше 8 символів")
+      .regex(
+        passwordRequirements,
+        "Пароль має містити велику літеру, малу літеру, цифру та спеціальний символ"
+      ),
+    confirm_password: z
+      .string()
+      .min(8, "Пароль повинен містити щонайменше 8 символів"),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message:
+      "Паролі не співпадають. Будь ласка, введіть однакові паролі в обидва поля",
+    path: ["confirm_password"],
+  });
+export type RestorePasswordFormValues = z.infer<typeof restorePasswordSchema>;
+
+export function RestorePassword() {
+  const restoreTokenValidQuery = useValidToken();
+  const [params] = useSearchParams();
+  const form = useForm<RestorePasswordFormValues>({
+    resolver: zodResolver(restorePasswordSchema),
+    mode: "onChange",
+    defaultValues: {
+      password: "",
+      confirm_password: "",
+    },
+  });
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const handleRestorePassword = async (data: RestorePasswordFormValues) => {
+    try {
+      console.log(data);
+    } catch (error: unknown) {
+      form.setError("password", {
+        type: "manual",
+        message: error instanceof Error ? error.message : "Невідома помилка",
+      });
+    }
+  };
+
+  useEffect(() => {
+    const token = params.get("token");
+    if (token) {
+      restoreTokenValidQuery.mutate({
+        token,
+      })
+    }
+  }, [params, restoreTokenValidQuery]);
+
+  return (
+    <div className="px-2 mx-auto flex items-center justify-center h-screen">
+      <Form form={form} onSubmit={handleRestorePassword}>
+        <Form.Header title="Відновлення паролю" />
+        <Form.Body>
+          <RestorePasswordForm form={form} />
+        </Form.Body>
+        <Form.Footer>
+          <Button disabled={!isValid || isSubmitting}>Зберегти пароль</Button>
+        </Form.Footer>
+      </Form>
+    </div>
+  );
+}
