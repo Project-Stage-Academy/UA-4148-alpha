1a56b22612e321abf50425a62585728550e37d32
DemaPy <benderdema@gmail.com>
2025-08-07 20:04:30 +0200

    feat: add function to validate if token is correct

--- frontend/src/hooks/useValidToken.ts
+++ frontend/src/hooks/useValidToken.ts
@@ -1,19 +1,32 @@
-import { validateTokenRestorePassword } from "@/api/validateTokenRestorePassword";
+import {
+  validateTokenRestorePassword,
+  type RestoreTokenData,
+  type RestoreTokenResponse,
+} from "@/api/validateTokenRestorePassword";
 import { useMutation } from "@tanstack/react-query";
+import type { AxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 
 export function useValidToken() {
   const navigate = useNavigate();
-  return useMutation({
+  return useMutation<
+    RestoreTokenResponse,
+    AxiosError<{ detail: string }>,
+    RestoreTokenData
+  >({
     mutationFn: validateTokenRestorePassword,
     onSuccess: (data) => {
       if (!data.valid) {
-        navigate("/signin");
+        navigate("/forgot-password");
         return;
       }
     },
     onError: (error) => {
-      console.error("Error validating restore token:", error);
+      console.error(error);
+      if (error.response?.status === 500) {
+        throw new Error("Серверна помилка, спробуйте пізніше");
+      }
+      throw new Error("Щось пішло не так, спробуйте ще раз");
     },
   });
 }
