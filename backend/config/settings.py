1cde542f660e5f6f8e77821318a393edf5a50224
koskin <kostia.zeykin@gmail>
2025-08-12 13:13:32 +0300

    removed duplicates rest_framework and rest_framework_simplejwt in settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,8 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'corsheaders',
-    'rest_framework',
-    'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
 
     'users',
