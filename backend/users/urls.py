7ff2e4a00476ea77c355bc6b2302c98cdce53d0b
koskin <kostia.zeykin@gmail>
2025-08-13 15:21:25 +0300

    add comments in users/urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,6 +7,6 @@ router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('users/login/', UserViewSet.as_view({'post': 'login'}), name='user-login'),
-    path('users/register/', UserViewSet.as_view({'post': 'register'}), name='user-register'),
+    path('users/login/', UserViewSet.as_view({'post': 'login'}), name='user-login'), # Maybe this code is not neededbecause we use ViewSet with @action and connect via DefaultRouter?
+    path('users/register/', UserViewSet.as_view({'post': 'register'}), name='user-register'), # Maybe this code is not neededbecause we use ViewSet with @action and connect via DefaultRouter?
 ]
