cbde687d20e1904923b60dab47b2c0af1262e806
Vlad <daniil.medencev01@gmail.com>
2025-08-13 20:53:51 +0300

    draft

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -1,3 +1,28 @@
+from rest_framework import serializers
+from .models import Subscription
 
-class ProjectSerializer:
-    pass
+class SubscriptionCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ['investor', 'project', 'share', 'investment_amount']
+
+    def validate(self, attrs):
+        instance = Subscription(**attrs)
+
+        if self.instance:
+            instance.pk = self.instance.pk
+
+        try:
+            instance.full_clean()
+        except serializers.ValidationError:
+            raise
+        except Exception as e:
+            from django.core.exceptions import ValidationError as DjangoValidationError
+            if isinstance(e, DjangoValidationError):
+                raise serializers.ValidationError(e.message_dict)
+            raise
+
+        return attrs
+
+    def create(self, validated_data):
+        return Subscription.objects.create(**validated_data)
