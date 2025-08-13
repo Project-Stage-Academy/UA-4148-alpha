b659a6a6f47a7ffd5071db884f9793bd60e1d709
koskin <kostia.zeykin@gmail>
2025-08-13 19:41:48 +0300

    added class Project to project/models as a stub

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Project(models.Model):
+    """Model representing a project."""
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
