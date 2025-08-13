afd38c07718bebdacbdbeed5639425ec404ef3f7
koskin <kostia.zeykin@gmail>
2025-08-13 12:38:48 +0300

    add email_activation.py in users/utils

---
+++ backend/users/utils/email_activation.py
@@ -0,0 +1,38 @@
+from django.core import signing
+from datetime import timedelta
+from django.utils import timezone
+from users.models import UserProfile
+from django.conf import settings
+
+def generate_activation_token(user):
+    """
+    Generate email confirmation token.
+    Token is valid for 24 hours.
+    """
+    data = {
+        "user_id": user.id,
+        "expires_at": (timezone.now() + timedelta(hours=24)).timestamp()
+    }
+    token = signing.dumps(data, key=settings.SECRET_KEY)
+    return token
+
+def verify_activation_token(token):
+    """
+    Check email confirmation token.
+    Returns user if token is valid, otherwise None + message.
+    """
+    try:
+        data = signing.loads(token, key=settings.SECRET_KEY)
+    except signing.BadSignature:
+        return None, "Invalid token"
+
+    expires_at = timezone.datetime.fromtimestamp(data["expires_at"], tz=timezone.utc)
+    if timezone.now() > expires_at:
+        return None, "The token has expired"
+
+    user_id = data["user_id"]
+    user = UserProfile.objects.filter(id=user_id).first()
+    if not user:
+        return None, "User not found"
+
+    return user, None
