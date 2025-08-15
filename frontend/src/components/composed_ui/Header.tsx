11238fcac8da9943d16d51a142186410fd620e31
DemaPy <benderdema@gmail.com>
2025-08-15 21:48:36 +0200

    feat: makes header adapt to screen size

--- frontend/src/components/composed_ui/Header.tsx
+++ frontend/src/components/composed_ui/Header.tsx
@@ -2,16 +2,26 @@ import { Navigation } from "./Navigation";
 import { AuthButtons } from "./AuthButtons";
 import { Logo } from "./Logo";
 import { Search } from "../composed/Search";
+import { useDesktop } from "@/hooks/useDesktop";
+import { MenuIcon } from "@/icons/MenuIcon";
 
 export const Header = () => {
+  const isDesktop = useDesktop();
   return (
     <header className="bg-main-white border-b border-inactive-60">
-      <div className="flex justify-between py-4 px-6 container mx-auto">
+      <div className="flex justify-between py-4 px-6 mx-auto">
         <Logo />
         <div className="flex items-center gap-6">
-          <Navigation />
-          <Search />
-          <AuthButtons />
+          {isDesktop && (
+            <>
+              <Navigation />
+              <Search />
+            </>
+          )}
+          <div className="flex items-center gap-6">
+            <AuthButtons />
+            {!isDesktop && <MenuIcon />}
+          </div>
         </div>
       </div>
     </header>
