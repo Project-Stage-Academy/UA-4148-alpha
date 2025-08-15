04a8b06211ba69f55b3bee33a97779df2297a7a6
DemaPy <benderdema@gmail.com>
2025-08-15 21:59:45 +0200

    feat: make padding smaller for header on desktop

--- frontend/src/components/composed_ui/Header.tsx
+++ frontend/src/components/composed_ui/Header.tsx
@@ -4,12 +4,18 @@ import { Logo } from "./Logo";
 import { Search } from "../composed/Search";
 import { useDesktop } from "@/hooks/useDesktop";
 import { MenuIcon } from "@/icons/MenuIcon";
+import { cn } from "@/libs/utils";
 
 export const Header = () => {
   const isDesktop = useDesktop();
   return (
     <header className="bg-main-white border-b border-inactive-60">
-      <div className="flex justify-between py-4 px-6 mx-auto">
+      <div
+        className={cn("flex justify-between max-w-[1304px] py-4 mx-auto", {
+          "px-6": isDesktop,
+          "px-4": !isDesktop,
+        })}
+      >
         <Logo />
         <div className="flex items-center gap-6">
           {isDesktop && (
