fdb2be41a6e5ee348d63744171ec2b18eb6a73fe
koskin <kostia.zeykin@gmail>
2025-08-16 13:27:57 +0300

    added alt="email-activation" for email activation tokens

--- backend/users/utils/email_activation.py
+++ backend/users/utils/email_activation.py
@@ -1,5 +1,4 @@
 from django.core import signing
-from django.utils import timezone
 from users.models import UserProfile
 
 def generate_activation_token(user):
@@ -8,7 +7,7 @@ def generate_activation_token(user):
     Token is valid for 24 hours.
     """
     data_for_activation = {"user_id": user.id}
-    signer = signing.TimestampSigner()
+    signer = signing.TimestampSigner(salt="email-activation")
     token = signer.sign_object(data_for_activation)
     return token
 
@@ -18,7 +17,7 @@ def verify_activation_token(token, max_age = 86400): # max_age = 24 hours in sec
     Returns user if token is valid, otherwise None + message.
     """
     
-    signer = signing.TimestampSigner()
+    signer = signing.TimestampSigner(salt="email-activation")
     
     try:
         data_for_activation = signer.unsign_object(token, max_age = max_age)
