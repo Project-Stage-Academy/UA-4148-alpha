655dcc9da0e6d4355e53e40f1272a4ff1533b864
Slava <80632445+DemaPy@users.noreply.github.com>
2025-08-16 17:59:30 +0200

    135 frontend adapt header to mobile devices (#136)
    * feat: add conditional rendering
    
    * feat: add new icons
    
    * feat: makes header adapt to screen size
    
    * feat: make padding smaller for header on desktop

--- frontend/src/components/composed_ui/Header.tsx
+++ frontend/src/components/composed_ui/Header.tsx
@@ -2,16 +2,32 @@ import { Navigation } from "./Navigation";
 import { AuthButtons } from "./AuthButtons";
 import { Logo } from "./Logo";
 import { Search } from "../composed/Search";
+import { useDesktop } from "@/hooks/useDesktop";
+import { MenuIcon } from "@/icons/MenuIcon";
+import { cn } from "@/libs/utils";
 
 export const Header = () => {
+  const isDesktop = useDesktop();
   return (
     <header className="bg-main-white border-b border-inactive-60">
-      <div className="flex justify-between py-4 px-6 container mx-auto">
+      <div
+        className={cn("flex justify-between max-w-[1304px] py-4 mx-auto", {
+          "px-6": isDesktop,
+          "px-4": !isDesktop,
+        })}
+      >
         <Logo />
         <div className="flex items-center gap-6">
-          <Navigation />
-          <Search />
-          <AuthButtons />
+          {isDesktop && (
+            <>
+              <Navigation />
+              <Search />
+            </>
+          )}
+          <div className="flex items-center gap-6">
+            <AuthButtons />
+            {!isDesktop && <MenuIcon />}
+          </div>
         </div>
       </div>
     </header>
