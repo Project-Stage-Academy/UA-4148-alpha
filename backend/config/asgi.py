d90ed96804ac1802f8cabf57aa45ca5c3d81ce66
koskin <kostia.zeykin@gmail>
2025-08-17 18:11:21 +0300

    added middleware connections to asgi.py

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -13,6 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
+from users.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -24,7 +25,10 @@ application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            JWTAuthMiddleware(
+                AuthMiddlewareStack(
+                    URLRouter(websocket_urlpatterns))  
+            )
         ),
     }
 )
