97fc2e4c5e6cbbec62d12f9acc0d513506e03874
DemaPy <benderdema@gmail.com>
2025-08-18 18:06:37 +0200

    feat: add switch_to_role_not_exist test case

--- backend/tests/test_role.py
+++ backend/tests/test_role.py
@@ -47,6 +47,22 @@ class TestUserRole:
         user.refresh_from_db()
         assert user.role == startupRole
 
+    def test_switch_to_role_not_exist(self):
+        userData = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password": "StrongPassw0rd!",
+            "first_name": "John",
+            "last_name": "Doe",
+            "role": None
+        }
+        user = UserProfile.objects.create_user(**userData)
+        self.client.force_authenticate(user=user)
+    
+        url = reverse('user-switch-role')
+        response = self.client.post(url, {"role_id": 2}, format='json')
+        assert response.status_code == 404
+
     def test_create_not_allowed_roles(self):
         data = {"role": "not_allowed_role_to_create"}
         response = self.client.post(self.url, data, format='json')
