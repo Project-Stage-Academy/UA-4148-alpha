246cb51f19b75460afc3f60af950b9fa8afb797f
koskin <kostia.zeykin@gmail>
2025-08-20 08:26:43 +0300

    fixed type hints

--- backend/users/utils/email_utils.py
+++ backend/users/utils/email_utils.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 
-def send_activation_email(token=str, recipient_email=str, frontend_url=str):
+def send_activation_email(token: str, recipient_email: str, frontend_url: str):
     """
     Sends an activation email with HTML and plain text versions.
     """
