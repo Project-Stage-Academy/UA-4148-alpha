c30beacd29749029cda50efa8fce70d6c76c23a0
Halyna Fedkiv <haliafedkiv@gmail.com>
2025-08-20 19:51:00 +0300

    chore(docker): set DJANGO_SETTINGS_MODULE env for web service

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from users.middleware import JWTAuthMiddleware
 from communications.routing import websocket_urlpatterns
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter(
     {
