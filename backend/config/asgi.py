ed657b79e9b002f397d82aee7ca4cbb9db7e4d43
DemaPy <benderdema@gmail.com>
2025-08-21 07:37:03 +0200

    fix: initialize django app before everything

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,16 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from users.middleware import JWTAuthMiddleware
 from communications.routing import websocket_urlpatterns
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter(
     {
