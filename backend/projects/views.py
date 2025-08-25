41566ea4660ae82c7bbc4a88f18252751f708191
koskin <kostia.zeykin@gmail>
2025-08-25 16:37:28 +0300

    add class ProjectViewSet in projects/views with methods follow_projects and unfollow_project

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .serializers import ProjectSerializer
+from .models import StartupProject
+from profiles.models import InvestorProfile
 
 
 class ProjectViewSet(viewsets.ViewSet):
@@ -32,3 +34,53 @@ class ProjectViewSet(viewsets.ViewSet):
         return Response(
             {"message": f"Subscribed to project {pk}"}, status=status.HTTP_201_CREATED
         )
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """ ViewSet for listing, retrieving, and subscribing/following projects."""
+    
+    queryset = StartupProject.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+    
+    # action "follow" for saving projects
+    @action(detail=True, methods=["post"], url_path="follow")
+    def follow_projects(self, request, pk=None):
+        """Allow an authenticated investor to follow (save) a startup project."""
+        
+        try:
+            investor_profile = InvestorProfile.objects.get(user=request.user)
+            project = self.get_project
+            investor_profile.saved_projects.add(project)
+            
+            return Response(
+                {"message": f"Project {project.id} has been saved to your profile."},
+                status=status.HTTP_201_CREATED,
+            )
+        except InvestorProfile.DoesNotExist:
+            return Response(
+                {"error": "You must be an investor to save projects."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+            
+    # action "unfollow" to remove a project from saved
+    @action(detail=True, method=["post"], url_path="unfollow")
+    def unfollow(self, request, pk=None):
+        """
+        Allow an authenticated investor to unfollow (remove) a startup project.
+        """
+        
+        try:
+            investor_profile = InvestorProfile.objects.get(user=request.user)
+            project = self.get_project()
+            
+            investor_profile.saved_projects.remove(project)
+            
+            return Response(
+                {"message": f"Project {project.id} has been removed from your saved list."},
+                status=status.HTTP_200_OK,
+            )
+        except InvestorProfile.DoesNotExist:
+            return Response(
+                {"error": "You must be an investor to remove saved projects."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
