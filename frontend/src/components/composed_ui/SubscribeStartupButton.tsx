5456cd35998489c573e0bc3910dd14046eeb3ff2
DemaPy <benderdema@gmail.com>
2025-08-29 19:36:34 +0200

    feat: render subscribe button only for investors

--- frontend/src/components/composed_ui/SubscribeStartupButton.tsx
+++ frontend/src/components/composed_ui/SubscribeStartupButton.tsx
@@ -1,15 +1,22 @@
 import { Heart } from "lucide-react";
 import { Button } from "../ui/button";
+import { useAuthContext } from "@/hooks/useAuthContext";
 
 interface SubscribeStartupButtonProps {
   id: number;
 }
 
 const SubscribeStartupButton = ({ id }: SubscribeStartupButtonProps) => {
+  const { user } = useAuthContext();
   const handleSubscribe = () => {
     alert("Subscribed to sturtup: " + id);
   };
 
+  if (user?.role != "investor") {
+    return;
+  }
+
+  // TODO: check if investor subscribed for this startup
   const subscribed = true;
   return (
     <Button variant={"tertiary"} onClick={handleSubscribe}>
