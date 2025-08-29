924ca4d44f59f45320e84e6345621f9bfe289f5e
DemaPy <benderdema@gmail.com>
2025-08-29 18:19:46 +0200

    feat: add interceptor for axios

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -8,4 +8,16 @@ const axiosInstance = axios.create({
   },
 });
 
+
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  })
+
 export default axiosInstance;
