34531a4a760b0ad85f796b258e4c67a773de406a
Vlad <daniil.medencev01@gmail.com>
2025-08-30 17:43:35 +0300

    SchemaView

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -43,14 +43,16 @@ urlpatterns = [
     path("", include("projects.urls")),
     path("api/", include(api_urlpatterns)),
     path(
-        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+        "swagger<format>/", SchemaView.without_ui(cache_timeout=0), name="schema-json"
     ),
     path(
         "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
+        SchemaView.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(
+        "redoc/", SchemaView.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
     path("chat/", include("communications.urls")),
 ]
 
