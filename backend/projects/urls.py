e47ff4b66565ec9813c4ee8a450352dec87e9fbb
koskin <kostia.zeykin@gmail>
2025-09-02 11:40:53 +0300

    change route in backend\projects\urls.py for right endpoint

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ProjectViewSet
 
 router = DefaultRouter()
-router.register(r"projects", ProjectViewSet, basename="project")
+router.register(r"", ProjectViewSet, basename="project")
 
 urlpatterns = [
     path("", include(router.urls)),
