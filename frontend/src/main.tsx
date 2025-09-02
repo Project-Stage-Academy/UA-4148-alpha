2ee6ddbed70c9c534c538dfea28c9bbeebbf4a21
DemaPy <benderdema@gmail.com>
2025-09-02 16:18:37 +0200

    feat: add Toaster component to root tree

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Router } from "./router";
 import { Providers } from "./components/service/providers";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster />
     <Providers>
       <Router />
     </Providers>
