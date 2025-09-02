664761a8714a22b05bd9c830a3f25726b5ebe899
DemaPy <benderdema@gmail.com>
2025-09-02 16:35:33 +0200

    revert websocket changes

--- frontend/src/components/service/providers/index.tsx
+++ frontend/src/components/service/providers/index.tsx
@@ -2,14 +2,11 @@ import type { PropsWithChildren } from "react";
 import { queryClient } from "./QueryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./AuthProvider";
-import { WebSocketProvider } from "./WebSocket";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <AuthProvider>
-      <WebSocketProvider>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      </WebSocketProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </AuthProvider>
   );
 }
