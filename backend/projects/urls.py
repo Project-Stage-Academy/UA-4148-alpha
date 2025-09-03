fa719f556e8b3bd3aa417a25fe9bd59e99c96282
koskin <kostia.zeykin@gmail>
2025-09-03 11:37:10 +0300

    add router in porjects/urls

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectViewSet
+from .views import ProjectViewSet, SavedProjectsList
 
 router = DefaultRouter()
 router.register(r"", ProjectViewSet, basename="project")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("investor/saved-projects/", SavedProjectsList.as_view(), name="investor-saved-projects"),
 ]
